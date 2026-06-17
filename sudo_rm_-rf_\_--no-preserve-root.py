91430977ddc49975fae30121a4bc29d71a1174fd
victhormedina-blip <victhor.medina@estudante.ifms.edu.br>
2026-06-17 14:56:09 -0400

    sudo pacman -Syu

--- sudo_rm_-rf_\_--no-preserve-root.py
+++ sudo_rm_-rf_\_--no-preserve-root.py
@@ -1,6 +1,8 @@
+#!/usr/bin/env pybricks-micropython
 from pybricks.hubs import EV3Brick
 from pybricks.ev3devices import Motor, InfraredSensor
 from pybricks.parameters import port
+from pybricks.tools import wait
 
 ev3 = EV3Brick
 mte = Motor(Port.B)
@@ -14,12 +16,15 @@ d3 = 0
 vl = 0 #constante que define a velocidade dos motores
 gb = 0 #o quanto o robô deve virar para fazer a próxima leitura de distancia
 ag = 0 #alcance da garra
+gn = gb * -1
 while True:
     d3 = sri.distance()
-    if d3> d2 && d1 > d2:
+    if d3> d2 & d1 > d2:
+        mte.run_angle(speed=vl, rotation_angle=gn, wait=False)
+        mtd.run_angle(speed=vl, rotation_angle=gb)
         while sri.distance() >= ag:
             mte.run_angle(speed=vl, rotation_angle=gb, wait=False)
-            mtd.run_angle(speed=vl, rotation_angle=-gb)
+            mtd.run_angle(speed=vl, rotation_angle=gb)
         #faça a garra pegar a bola
         d1 = 0
         d2 = 0
@@ -28,4 +33,4 @@ while True:
     d1 = d2
     d2 = d3
     mte.run_angle(speed=vl, rotation_angle=gb, wait=False)
-    mtd.run_angle(speed=vl, rotation_angle=-gb)
+    mtd.run_angle(speed=vl, rotation_angle=gn)
