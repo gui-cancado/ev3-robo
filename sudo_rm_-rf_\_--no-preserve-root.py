a36a7c5ec50f34651d6457b8e67cfcaffe8b532e
victhormedina-blip <victhor.medina@estudante.ifms.edu.br>
2026-06-17 08:45:17 -0400

    sudo rm -rf / --no-preserve-root

---
+++ sudo_rm_-rf_\_--no-preserve-root.py
@@ -0,0 +1,31 @@
+from pybricks.hubs import EV3Brick
+from pybricks.ev3devices import Motor, InfraredSensor
+from pybricks.parameters import port
+
+ev3 = EV3Brick
+mte = Motor(Port.B)
+mtd = Motor(Port.C)
+sri = InfraredSensor(port.S4)
+
+d1 = 0
+d2 = 0
+d3 = 0
+
+vl = 0 #constante que define a velocidade dos motores
+gb = 0 #o quanto o robô deve virar para fazer a próxima leitura de distancia
+ag = 0 #alcance da garra
+while True:
+    d3 = sri.distance()
+    if d3> d2 && d1 > d2:
+        while sri.distance() >= ag:
+            mte.run_angle(speed=vl, rotation_angle=gb, wait=False)
+            mtd.run_angle(speed=vl, rotation_angle=-gb)
+        #faça a garra pegar a bola
+        d1 = 0
+        d2 = 0
+        d3 = 0
+
+    d1 = d2
+    d2 = d3
+    mte.run_angle(speed=vl, rotation_angle=gb, wait=False)
+    mtd.run_angle(speed=vl, rotation_angle=-gb)
